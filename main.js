482c7a3c21e46f3a1f7f9f297554874c30081b8d
andrewdreamer13 <isaevandrey9@gmail.com>
2024-12-08 21:45:33 +0200

    added sound

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
-window.addEventListener('pointerdown', (e) => {
-  const drop = document.createElement('div');
-   drop.classList.add('drop');
-   document.body.append(drop)
-   
-   drop.style.cssText = 
-   `
-    left: ${e.pageX - drop.getBoundingClientRect().width / 2}px;
-   top: ${e.pageY - drop.getBoundingClientRect().height / 2}px;
-    background-color: rgb(
-     ${getRandomInt(0,250)},
-     ${getRandomInt(0,250)},
-     ${getRandomInt(0,250)}
-    );
-   `
- })
- 
- function getRandomInt(min,max) {
-   return Math.round(Math.random() * (max - min) + min);
- }
+const title = document.querySelector('.title');
+
+window.addEventListener("pointerdown", (e) => {
+
+  const bellSound = document.createElement("audio");
+  bellSound.src = "sound/bubbles.mp3";
+  document.body.append(bellSound);
+
+  const drop = document.createElement("div");
+  drop.classList.add("drop");
+  document.body.append(drop);
+  const color = `rgb(${getRandomInt(0, 250)}, ${getRandomInt(0, 250)}, ${getRandomInt(0, 250)})`;
+
+  drop.style.cssText = `
+    left: ${e.pageX - drop.offsetWidth / 2}px;
+    top: ${e.pageY - drop.offsetHeight / 2}px;
+    background-color: ${color}
+   `;
+   title.style.color = color;
+  playSound(bellSound);
+});
+
+function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function playSound(sound) {
+  sound.play();
+  sound.currentTime = 0;
+}
+
+
+ //  left: ${e.pageX - drop.getBoundingClientRect().width / 2}px;
+  //  top: ${e.pageY - drop.getBoundingClientRect().height / 2}px;
